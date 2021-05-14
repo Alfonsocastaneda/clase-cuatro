583ca53d328439ab0402031ad32927b4cd455f6d
Alfonso Belky <belkyalf@gmail.com>
2021-05-13 21:34:08 -0500

    case 4

---
+++ Empleados/empleado.py
@@ -0,0 +1,20 @@
+class Empleado:
+    def __init__(self):
+        self.__nombre = None
+        self.apellido = None
+        self.cargo = None
+        self.salario = None
+    def getNombre(self):
+        return self.__nombre
+
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+
+    def __str__(self):
+        return str("nombre: {} \n"
+        "apellido: {} \n"
+        "salario: {} ").format(
+            self.__nombre,
+            self.apellido,
+            self.cargo,
+            self.salario)
